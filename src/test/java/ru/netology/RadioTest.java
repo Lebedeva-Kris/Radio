7ec9751d69c5a57ed93d29f15aa083d5e83fbb8a
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-04 01:46:03 +0300

    initial test added

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,7 +1,16 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
+
+    @Test
+    void shouldChangeCurrentRadioStation() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioStation());
+        radio.setCurrentRadioStation(6);
+        assertEquals(7, radio.getCurrentRadioStation());
+    }
 
 }
